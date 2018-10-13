9bf40f46e37e332d603df3a101ba7912817c7eb3
Daniel <dreyeslastiri@users.noreply.github.com>
2018-10-13 02:24:43 +0200

    Update 01_initialize_lotkavolterra.py

--- vorsetmembership/_tests/01_initialize_lotkavolterra.py
+++ vorsetmembership/_tests/01_initialize_lotkavolterra.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 @author: Daniel Reyes Lastiri
-Set-membership evaluation for Lotka-Volterra
+Set-membership evaluation for Lotka-Volterra.
+Test. Initialize parameter space.
 """
 
 import numpy as np
@@ -30,23 +31,19 @@ def lotka_volterra(t,y0,u,ut,p, b,d):
 # ---- ARTIFICIAL DATA ----
 p = {'a':1,
      'c':1}
-b = 0.01
-d = 0.02
+b,d = 0.01, 0.02 
 y0 = (50,50)
 tsim = np.linspace(0,7,num=7*24+1)
 ysim = lotka_volterra(tsim,y0,None,None,p,b,d)
 idata = np.arange(1*24,7*24,1*24)
-tdata = tsim[idata]
-ydata = ysim[idata]
+tdata, ydata = tsim[idata], ysim[idata]
 # -- Artificial bounds --
 errmin1 = np.average(ydata[:,0])/4 * (1 + rnd.random_sample(ydata.shape[0]))
 errmax1 = np.average(ydata[:,0])/4 * (1 + rnd.random_sample(ydata.shape[0]))
 errmin2 = np.average(ydata[:,1])/4 * (1 + rnd.random_sample(ydata.shape[0]))
 errmax2 = np.average(ydata[:,1])/4 * (1 + rnd.random_sample(ydata.shape[0]))
-
 ymin = np.stack(( ydata[:,0] - errmin1, ydata[:,1] - errmin2 ),axis=-1)
 ymax = np.stack(( ydata[:,0] + errmax1, ydata[:,1] + errmax2 ),axis=-1)
-
 errmin = np.stack((errmin1,errmin2),axis=-1)
 errmax = np.stack((errmax1,errmax2),axis=-1)
 errbars = np.stack((errmin,errmax))
@@ -67,56 +64,43 @@ pfn = init_results['pfn'] # normalized feasible points
 pun = init_results['pun'] # normalized unfeasible points
 minmax = init_results['norm_minmax'] # min and max used for normalization
 
-# Figure. Bounds, feasible example and original simulation
-fig1 = plt.figure(1)
+# Figure 1. Bounds, feasible example and original simulation
+fig1 = plt.figure('Bounds')
 x1 = [tsim,]
 y1a = [ysim[:,0],]
 y1b = [ysim[:,1],]
-ax1a = plt.subplot2grid((8,1), (0, 0), rowspan=3)
-ax1a = plot_lines(
-    ax1a,x1,y1a, xlabel=r'$t$', ylabel=r'$x_{1}(\theta)$',
-    linestyle_list=['-',],
-    label_list = [r'$x_{1}$',]
-    )
+ax1a = plt.subplot2grid((6,1), (0, 0), rowspan=3)
+ax1a = plot_lines(ax1a,x1,y1a,xlabel=r'$t\ [days]$',ylabel=r'$x_{1}(\theta)$',
+                  linestyle_list=['-',], label_list=['x1',])
 ax1a.errorbar(tdata,ydata[:,0], yerr=errbars[:,:,0], linestyle='',
               capthick=1, elinewidth=1, ecolor='#2d8ce5')
 ax1a.set_xlim([0,7])
-ax1a.set_ylim([0,100])
-
-ax1b = plt.subplot2grid((8,1), (4, 0), rowspan=3)
-ax1b = plot_lines(
-    ax1b,x1,y1b, xlabel=r'$t$', ylabel=r'$x_{2}(\theta)$',
-    linestyle_list=['-',],
-    label_list = [r'$x_{2}$',]
-    )
+ax1a.set_ylim([0,150])
+ax1a.legend().set_visible(False)
+ax1b = plt.subplot2grid((6,1), (3, 0), rowspan=3)
+ax1b = plot_lines(ax1b,x1,y1b,xlabel=r'$t\ [days]$',ylabel=r'$x_{2}(\theta)$',
+                  linestyle_list=['-',], label_list=['x2',])
 ax1b.errorbar(tdata,ydata[:,1], yerr=errbars[:,:,1], linestyle='', capthick=1,
-    elinewidth=1, ecolor='#2d8ce5'
-    )
-
+              elinewidth=1, ecolor='#2d8ce5')
 ax1b.set_xlim([0,7])
 ax1b.set_ylim([0,200])
+ax1b.legend().set_visible(False)
 fig1.set_size_inches(8.5/2.54 , 7.5/2.54) # Required 1 column width
+fig1.tight_layout()
 
-# Figure. Parameter space
-fig2 = plt.figure(2)
+# Figure 2. Parameter space
+fig2 = plt.figure('Initialized')
 ax2 = plt.subplot2grid((4,1), (0, 0), rowspan=3)
-ax2 = plot_scatter(
-    ax2,[pun,pfn],
-    xlabel=r'$b$ (normalized) [{0:.4f}, {1:.4f}]'.format(minmax[0,0],minmax[1,0]),
-    ylabel=r'$d$ (normalized) [{0:.4f}, {1:.4f}]'.format(minmax[0,1],minmax[1,1]),
-    marker_list=['x','o'],
-    label_list = ['Unfeasible','Feasible']
+ax2 = plot_scatter(ax2,[pun,pfn],
+    xlabel=r'$b$' ' (normalized) \n [{0:.4f}, {1:.4f}]'.format(
+            minmax[0,0],minmax[1,0]),
+    ylabel=r'$d$' ' (normalized) \n [{0:.4f}, {1:.4f}]'.format(
+            minmax[0,1],minmax[1,1]),
+    marker_list=['x','o'], label_list=['Unfeasible','Feasible'], legend_pad=0.25
     )
-
 ax2.locator_params(axis='both',nbins=4)
 ax2.set_aspect('equal')
-#ax2.set_xlim(-2.0,3.0)
-#ax2.set_ylim(-2.0,3.0)
 ax2.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
 ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-fig2.set_size_inches(7.5/2.54 , 7.5/2.54) # Required 1 column width
-
-
-
-
-
+fig2.set_size_inches(7.5/2.54 , 7.5/2.54) # 1 column width
+fig2.tight_layout()
